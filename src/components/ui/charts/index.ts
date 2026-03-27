26ddbda99eba23c6ca08d5c0e11fe5b6253dac5c
Zephyrxx0 <tronlegacy12321@gmail.com>
2026-03-27 12:41:55 +0530

    chore(01-01): install recharts and create charts directory
    - Add recharts 3.8.1 dependency
    - Create src/components/ui/charts/ directory
    - Add barrel export file for chart components

---
+++ src/components/ui/charts/index.ts
@@ -0,0 +1,5 @@
+// Chart components barrel export
+export { ChartContainer } from './chart-container';
+export { LineChartWidget } from './line-chart';
+export { BarChartWidget } from './bar-chart';
+export { PieChartWidget } from './pie-chart';
